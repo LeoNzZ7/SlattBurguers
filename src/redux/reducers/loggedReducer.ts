5aed0243f3b554824d98b75403c1959ab1f2ee87
Leonardo Nunes Martinha <Leonardomartinha.dev@gmail.com>
2022-06-14 10:34:25 -0300

    Reducer

--- src/redux/reducers/loggedReducer.ts
+++ src/redux/reducers/loggedReducer.ts
@@ -3,11 +3,27 @@ import { createSlice } from "@reduxjs/toolkit";
 const slice = createSlice({
     name: 'logged',
     initialState: {
-        status: false
+        status: false,
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
     },
     reducers: {
         setLoggedStatus: (state, action) => {
             state.status = action.payload;
+        },
+        setLoggedFirstName: (state, action) => {
+            state.firstName = action.payload;
+        },
+        setLoggedLastName: (state, action) => {
+            state.lastName = action.payload;
+        },
+        setLoggedEmail: (state, action) => {
+            state.email = action.payload;
+        },
+        setLoggedPassword: (state, action) => {
+            state.password = action.payload;
         }
     }
 });
