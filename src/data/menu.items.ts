bec356d8540c72b8e357a2fed64738b58d927d9c
Leonardo Nunes Martinha <Leonardomartinha.dev@gmail.com>
2022-06-15 17:07:06 -0300

    Menu data

---
+++ src/data/menu.items.ts
@@ -0,0 +1,30 @@
+import burgue from '../assets/menu/burgue.png';
+import burgue2 from '../assets/menu/burgue2.png';
+import burgue3 from '../assets/menu/burgue3.png';
+import burgue4 from '../assets/menu/burgue4.png';
+import burgue5 from '../assets/menu/burgue5.png';
+import burgue6 from '../assets/menu/burgue6.png';
+import burgue7 from '../assets/menu/burgue7.png';
+import burgue8 from '../assets/menu/burgue8.png';
+import burgue9 from '../assets/menu/burgue9.png';
+import burgue10 from '../assets/menu/burgue10.png';
+
+type MenuItems = {
+    name: string,
+    img: string,
+    description: string,
+    price: number
+};
+
+export const MenuItem: MenuItems[] = [
+    {name: 'Slatt Bacon', description: '...', img: burgue, price: 25},
+    {name: 'Slatt Bacon', description: '...', img: burgue2, price: 25},
+    {name: 'Slatt Bacon', description: '...', img: burgue3, price: 25},
+    {name: 'Slatt Bacon', description: '...', img: burgue4, price: 25},
+    {name: 'Slatt Bacon', description: '...', img: burgue5, price: 25}, 
+    {name: 'Slatt Bacon', description: '...', img: burgue6, price: 25},
+    {name: 'Slatt Bacon', description: '...', img: burgue7, price: 25},
+    {name: 'Slatt Bacon', description: '...', img: burgue8, price: 25},  
+    {name: 'Slatt Bacon', description: '...', img: burgue9, price: 25},
+    {name: 'Slatt Bacon', description: '...', img: burgue10, price: 25}
+];
